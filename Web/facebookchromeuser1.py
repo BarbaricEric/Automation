e4b86e319eda8f0d6af58a611c14d8c3839573e4
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-12-15 06:02:17 -0800

    Add files via upload

---
+++ Web/facebookchromeuser1.py
@@ -0,0 +1,24 @@
+# Selenium, Google Chrome Web Driver, and Python installation required
+# Facebook login automation
+# Selenium documentation notes this method is bad practice and the company/oraganization API should be used instead
+
+#using webdriver from selenium module
+from selenium import webdriver
+
+#If not using a system path, link your chromedriver.exe (For non-Windows users, it's just called chromedriver):
+#variable = webdriver.Chrome(executable_path=r"C:\path\to\chromedriver.exe")
+#(Set executable_path to the location where your chromedriver is located.)
+
+#If you've placed chromedriver on your System Path, you can shortcut by just doing the following:
+#variable = webdriver.Chrome()
+
+#If still confused, refer to https://stackoverflow.com/questions/42478591/python-selenium-chrome-webdriver
+
+fb = webdriver.Chrome() 
+fb.get('https://facebook.com')
+username = fb.find_element_by_xpath('//*[@id="email"]')
+username.send_keys('USER ID FOR FACEBOOK ENTER HERE')
+password = fb.find_element_by_xpath('//*[@id="pass"]')
+password.send_keys('PASSWORD FOR FACEBOOK ENTER HERE')
+login = fb.find_element_by_xpath('//*[@id="u_0_b"]')
+login.click()
