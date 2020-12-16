40655abce84371423acd2dd6185a0b11395fdad6
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-12-16 06:37:03 -0800

    Update readCensusExcel.py

--- Excel/readCensusExcel.py
+++ Excel/readCensusExcel.py
@@ -1,14 +1,25 @@
 #! python3
 # readCensusEcel.py - Tabulates population and number of census tracts for each county
 
-import openxl, pprint
+import openpyxl, pprint
 print('Opening workbook...')
-wb = openpyxl.loada_workbook('censuspopdata.xlsx')
-sheet = wb['Population by Census Tract']
+wb = openpyxl.load_workbook('censuspopdata.xlsx')
+sheet = wb.get_sheet_by_name('Population by Census Tract')
 countyData = {}
 
+# Fill in countyData with each county's population and tracts.
 print('Reading rows...')
 for row in range(2, sheet.max_row + 1);
  state = sheet['B' + str(row)].value
  county = sheet['C' + str(row)].value
  pop = sheet['D' + str(row)].value
+ 
+# Make sure the key for this state exists.
+countyData.setdefault(state, {})
+# Make sure the key for this county in this state exists.
+countyData[state].setdefault(county, {'tracts': 0, 'pop': 0})
+
+# Each row represents one census tract, so increment by one.
+countyData[state][county]['tracts'] += 1
+# Increase the county pop by the pop in this census tract.
+countyData[state][county]['pop'] += int(pop)
